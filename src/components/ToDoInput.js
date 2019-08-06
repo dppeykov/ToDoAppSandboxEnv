d47cc4b3598ba35d30a243ac31c7c59bf0687211
Damyan Peykov <dppeykov@gmail.com>
2019-08-06 14:28:26 +0200

    Adding dividing border

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const ToDoInput = () => (
-  <div className="container d-flex align-items-center p-3">
-    <input
-      className="form-control rounded-pill w-75 ml-auto"
-      placeholder="What do you need to do today ..."
-    />
-    <button className="btn btn btn-success ml-3 mr-auto">+</button>
+  <div className="bg-dark border-top border-light">
+    <div className="container d-flex align-items-center p-3">
+      <input
+        className="form-control rounded-pill w-75 ml-auto"
+        placeholder="What do you need to do today ..."
+      />
+      <button className="btn btn btn-success ml-3 mr-auto">+</button>
+    </div>
   </div>
 );
 
